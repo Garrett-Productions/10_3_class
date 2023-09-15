7e15fb6e6927f9c6777997ee49297640ecb1486b
Garrett Turner <GarrettturnerProductions@gmail.com>
2023-09-15 12:25:43 -1000

    first commit

---
+++ 10_3_class.js
@@ -0,0 +1,21 @@
+// Programming isn't the solution it is the method we use to find a solution
+// Coding is a skill where you learn how to use different tools
+// tools are things like condtionals, for loops, whileloops, variables
+
+
+while(value !== 'C' && next !== null){ // in most of our while loops we may need more than 1 conditional
+
+}
+
+
+//to remove, set the previous nodes values to whatever the nedxt nodes value is
+//method called insert after 'T'.
+// WE use a while toop to find the 'T'
+
+// we set our current next which is T
+// temp = current.next
+// current.next = new_node
+// new.next = temp
+
+
+// an edge case is a way to try and protect your site. When we do an insert there's a bit of an edge case here.
